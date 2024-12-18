5924cbe4915a169ce7362655d77f1c448bb1be5c
sgd018 <samuel@sgd018.com>
2024-12-18 09:46:57 +0100

    Version 2

--- src/controladores/Inicio.java
+++ src/controladores/Inicio.java
@@ -4,7 +4,7 @@ public class Inicio {
 
 	public static void main(String[] args) {
 		
-		System.out.println("¡Hola, mundo desde Alumno A!");
+		System.out.println("¡Hola, mundo desde Alumno B!");
 
 	}
 
